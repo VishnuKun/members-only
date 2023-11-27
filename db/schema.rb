b99acc179f0a01318c1a5aa3b120402ef15cb4d1
Vishnu <vishnukun954AV@protonmail.com>
2023-11-27 23:07:51 +0545

    Added index for posts in views and commented duplicate email column in migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_27_130455) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_27_133454) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "user_id", null: false
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_27_130455) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
