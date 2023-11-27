2419d8e0cb710321f5ef7e38d922e5fc31385100
Vishnu <vishnukun954AV@protonmail.com>
2023-11-27 19:16:44 +0545

    Methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,55 @@
+# User Controller
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = 'User created successfully.'
+      redirect_to users_path
+    else
+      flash[:error] = 'There was a problem creating the user.'
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update
+      flash[:notice] = 'User has been updated'
+      redirect_to users_path
+    else
+      flash[:error] = 'User could not be found'
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
