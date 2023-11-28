082e216995373ea2325f5dc2e5c8955fc1a5dec4
Vishnu <vishnukun954AV@protonmail.com>
2023-11-28 10:49:32 +0545

    Added user column in posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_27_133454) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_28_050150) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "user_id", null: false
@@ -26,6 +26,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_27_133454) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,4 +46,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_27_133454) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "posts", "users"
 end
