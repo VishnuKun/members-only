296ce42b54247cba47bc2328498f35f02b3b3622
Vishnu <vishnukun954AV@protonmail.com>
2023-11-30 13:10:41 +0545

    Search bar now functional with body as well

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,9 @@ class Post < ApplicationRecord
   validates :title, :body, presence: true, length: { minimum: 1 }
 
   def self.search(query)
-    where('title LIKE ?', "%#{query}%")
+    title_matches = where('title LIKE ?', "%#{query}%")
+    body_matches = where('body LIKE ?', "%#{query}%")
+
+    title_matches + (body_matches - title_matches)
   end
 end
